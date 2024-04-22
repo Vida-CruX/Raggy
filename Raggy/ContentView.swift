4cc7b5e736de3bd75aa9c3117801871304ea9a5b
Vida-CruX <esrkforward@gmail.com>
2024-04-22 15:18:02 +0800

    Initial Commit

---
+++ Raggy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Raggy
+//
+//  Created by 陈鹤 on 22/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raggy/RaggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaggyApp.swift
+//  Raggy
+//
+//  Created by 陈鹤 on 22/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct RaggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
